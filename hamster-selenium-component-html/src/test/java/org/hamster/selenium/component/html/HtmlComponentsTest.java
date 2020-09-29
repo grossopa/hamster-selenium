8ba80951093e90be8c50a98ceeb2229efd361293
Jack Yin <grossopaforever@gmail.com>
2020-09-29 17:41:58 +0800

    WIP adding unit testing for html module

---
+++ hamster-selenium-component-html/src/test/java/org/hamster/selenium/component/html/HtmlComponentsTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright © 2020 the original author or authors.
+ *
+ * Licensed under the The MIT License (MIT) (the "License");
+ *  You may obtain a copy of the License at
+ *
+ *         https://mit-license.org/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.hamster.selenium.component.html;
+
+import org.hamster.selenium.component.html.factory.HtmlSelectFactory;
+import org.hamster.selenium.component.html.factory.HtmlTableFactory;
+import org.hamster.selenium.core.ComponentWebDriver;
+import org.hamster.selenium.core.component.WebComponent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests for {@link HtmlComponents}
+ *
+ * @author Jack Yin
+ * @since 1.0
+ */
+class HtmlComponentsTest {
+
+    HtmlComponents testSubject;
+    ComponentWebDriver driver = mock(ComponentWebDriver.class);
+    WebElement element = mock(WebElement.class);
+    WebComponent component = mock(WebComponent.class);
+
+    @BeforeEach
+    void setUp() {
+        when(component.getElement()).thenReturn(element);
+        testSubject = new HtmlComponents();
+    }
+
+
+    @Test
+    void toFormField() {
+        when(component.to(any())).thenReturn(new HtmlFormField(element, driver));
+        testSubject.setContext(component, driver);
+        assertEquals(HtmlFormField.class, testSubject.toFormField().getClass());
+    }
+
+    @Test
+    void toTable() {
+        when(element.getTagName()).thenReturn("table");
+        HtmlTable htmlTable = new HtmlTable(element, driver);
+        when(component.to(any(HtmlTableFactory.class))).thenReturn(htmlTable);
+        testSubject.setContext(component, driver);
+        assertEquals(HtmlTable.class, testSubject.toTable().getClass());
+    }
+
+    @Test
+    void toSelect() {
+        when(element.getTagName()).thenReturn("select");
+        HtmlSelect htmlSelect = new HtmlSelect(element, driver);
+        when(component.to(any(HtmlSelectFactory.class))).thenReturn(htmlSelect);
+        testSubject.setContext(component, driver);
+        assertEquals(HtmlSelect.class, testSubject.toSelect().getClass());
+    }
+
+    @Test
+    void html() {
+        assertNotNull(HtmlComponents.html());
+    }
+}
---
+++ hamster-selenium-component-html/src/test/java/org/hamster/selenium/component/html/HtmlTableTest.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright © 2020 the original author or authors.
+ *
+ * Licensed under the The MIT License (MIT) (the "License");
+ *  You may obtain a copy of the License at
+ *
+ *         https://mit-license.org/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.hamster.selenium.component.html;
+
+import org.hamster.selenium.core.ComponentWebDriver;
+import org.hamster.selenium.core.component.WebComponent;
+import org.hamster.selenium.core.component.api.TableRow;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.UnexpectedTagNameException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.*;
+
+/**
+ * Tests for {@link HtmlTable}
+ *
+ * @author Jack Yin
+ * @since 1.0
+ */
+class HtmlTableTest {
+
+    HtmlTable testSubject;
+    WebElement element = mock(WebElement.class);
+    ComponentWebDriver driver = mock(ComponentWebDriver.class);
+
+    WebElement headerRow1 = mock(WebElement.class);
+    WebElement headerRow2 = mock(WebElement.class);
+    WebElement header1 = mock(WebElement.class);
+    WebElement header2 = mock(WebElement.class);
+    WebElement header3 = mock(WebElement.class);
+
+    private WebElement createBodyRow(Integer index, String cell1, String cell2, String cell3) {
+        WebElement container = mock(WebElement.class);
+        WebElement cellElement1 = mock(WebElement.class);
+        WebElement cellElement2 = mock(WebElement.class);
+        WebElement cellElement3 = mock(WebElement.class);
+        when(cellElement1.getText()).thenReturn(index + "," + cell1);
+        when(cellElement2.getText()).thenReturn(index + "," + cell2);
+        when(cellElement3.getText()).thenReturn(index + "," + cell3);
+        when(container.findElements(testSubject.getColsLocator()))
+                .thenReturn(asList(cellElement1, cellElement2, cellElement3));
+        return container;
+    }
+
+    @BeforeEach
+    void setUp() {
+        when(element.getTagName()).thenReturn("table");
+        testSubject = new HtmlTable(element, driver);
+
+        // header row
+        when(header1.getText()).thenReturn("header 1");
+        when(header2.getText()).thenReturn("header 2");
+        when(header3.getText()).thenReturn("header 3");
+
+        when(headerRow1.findElements(testSubject.getHeaderColsLocator())).thenReturn(asList(header1, header2, header3));
+        when(element.findElements(eq(testSubject.getHeaderRowsLocator()))).thenReturn(asList(headerRow1, headerRow2));
+
+        List<WebElement> bodyContainers = asList(
+                createBodyRow(0, "cell 1 some text 123", "cell 2 some text 223", "cell 3"),
+                createBodyRow(1, "cell 1 some text 123", "cell 2 some text 223", "cell 3"),
+                createBodyRow(2, "cell 1 some text 123", "cell 2 some text 223", "cell 3"),
+                createBodyRow(3, "cell 1 some text 123", "cell 2 some text 223", "cell 3"),
+                createBodyRow(4, "cell 1 some text 123", "cell 2 some text 223", "cell 3"));
+        // body row
+        when(element.findElements(eq(testSubject.getRowsLocator()))).thenReturn(bodyContainers);
+    }
+
+    @Test
+    void constructorNotTable() {
+        WebElement element = mock(WebElement.class);
+        when(element.getTagName()).thenReturn("div");
+        assertThrows(UnexpectedTagNameException.class, () -> new HtmlTable(element, driver));
+    }
+
+    @Test
+    void getHeaderRow() {
+        TableRow row = testSubject.getHeaderRow();
+        assertNotNull(row);
+        assertEquals(3, row.getCells().size());
+        assertEquals(header1, row.getCells().get(0).getElement());
+        assertEquals(header2, row.getCells().get(1).getElement());
+        assertEquals(header3, row.getCells().get(2).getElement());
+    }
+
+    @Test
+    void getHeaderRows() {
+        List<TableRow> rows = testSubject.getHeaderRows();
+        assertNotNull(rows);
+        assertEquals(2, rows.size());
+        assertEquals(headerRow1, rows.get(0).getElement());
+        assertEquals(headerRow2, rows.get(1).getElement());
+    }
+
+    @Test
+    void getHeaderRowsEmpty() {
+        when(element.findElements(eq(testSubject.getHeaderRowsLocator()))).thenReturn(emptyList());
+        List<TableRow> rows = testSubject.getHeaderRows();
+        assertNotNull(rows);
+        assertEquals(0, rows.size());
+    }
+
+    @Test
+    void getHeaderLabels() {
+        List<String> labels = testSubject.getHeaderLabels();
+        assertEquals(3, labels.size());
+        assertTrue(labels.contains("header 1"));
+        assertTrue(labels.contains("header 2"));
+        assertTrue(labels.contains("header 3"));
+    }
+
+    @Test
+    void getHeaderLabelsEmpty() {
+        when(element.findElements(eq(testSubject.getHeaderRowsLocator()))).thenReturn(emptyList());
+        List<String> labels = testSubject.getHeaderLabels();
+        assertNotNull(labels);
+        assertEquals(0, labels.size());
+    }
+
+    @Test
+    void getBodyRow() {
+        List<TableRow> bodyRows = testSubject.getBodyRows();
+        assertEquals(5, bodyRows.size());
+    }
+
+    @Test
+    void getBodyRows() {
+        TableRow row = testSubject.getBodyRow(2);
+        assertEquals("2,cell 1 some text 123", row.getCell("header 1").getText());
+    }
+
+    @Test
+    void getBodyRowsNoSuchHeader() {
+        TableRow row = testSubject.getBodyRow(2);
+        assertThrows(NoSuchElementException.class, () -> row.getCell("header 888"));
+    }
+
+    @Test
+    void getHeaderRowsLocator() {
+        assertEquals("By.xpath: .//tr[./th]", testSubject.getHeaderRowsLocator().toString());
+    }
+
+    @Test
+    void getHeaderColsLocator() {
+        assertEquals("By.xpath: ./th", testSubject.getHeaderColsLocator().toString());
+    }
+
+    @Test
+    void getRowsLocator() {
+        assertEquals("By.xpath: .//tr[./td]", testSubject.getRowsLocator().toString());
+    }
+
+    @Test
+    void getColsLocator() {
+        assertEquals("By.xpath: ./td", testSubject.getColsLocator().toString());
+    }
+}
