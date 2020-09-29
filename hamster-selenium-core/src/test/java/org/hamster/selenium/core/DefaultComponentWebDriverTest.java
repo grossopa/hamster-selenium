36e0cf4b137c2214f4f23022584eba3e1ff293bb
Jack Yin <grossopaforever@gmail.com>
2020-09-29 14:44:34 +0800

    Add more test cases for core module

---
+++ hamster-selenium-core/src/test/java/org/hamster/selenium/core/DefaultComponentWebDriverTest.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright © 2020 the original author or authors.
+ *
+ * Licensed under the The MIT License (MIT) (the "License");
+ *  You may obtain a copy of the License at
+ *
+ *         https://mit-license.org/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.hamster.selenium.core;
+
+import org.hamster.selenium.core.component.WebComponent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests for {@link DefaultComponentWebDriver}
+ *
+ * @author Jack Yin
+ * @since 1.0
+ */
+class DefaultComponentWebDriverTest {
+
+    DefaultComponentWebDriver testSubject;
+    WebDriver driver = mock(WebDriver.class);
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new DefaultComponentWebDriver(driver);
+    }
+
+
+    @Test
+    void findComponents() {
+        WebElement mockElement1 = mock(WebElement.class);
+        WebElement mockElement2 = mock(WebElement.class);
+        when(driver.findElements(any())).thenReturn(Arrays.asList(mockElement1, mockElement2));
+        List<WebComponent> result = testSubject.findComponents(By.id("some"));
+        assertEquals(mockElement1, result.get(0).getElement());
+        assertEquals(mockElement2, result.get(1).getElement());
+    }
+
+    @Test
+    void findComponent() {
+        WebElement mockElement1 = mock(WebElement.class);
+        when(driver.findElement(any())).thenReturn(mockElement1);
+        WebComponent component = testSubject.findComponent(By.id("some"));
+        assertEquals(mockElement1, component.getElement());
+    }
+
+    @Test
+    void get() {
+        testSubject.get("some-url");
+        verify(driver, only()).get(eq("some-url"));
+    }
+
+    @Test
+    void getCurrentUrl() {
+        testSubject.getCurrentUrl();
+        verify(driver, only()).getCurrentUrl();
+    }
+
+    @Test
+    void getTitle() {
+        testSubject.getTitle();
+        verify(driver, only()).getTitle();
+    }
+
+    @Test
+    void findElements() {
+        WebElement mockElement1 = mock(WebElement.class);
+        WebElement mockElement2 = mock(WebElement.class);
+        when(driver.findElements(any())).thenReturn(Arrays.asList(mockElement1, mockElement2));
+        List<WebElement> result = testSubject.findElements(By.id("some"));
+        assertEquals(mockElement1, result.get(0));
+        assertEquals(mockElement2, result.get(1));
+    }
+
+    @Test
+    void findElement() {
+        WebElement mockElement1 = mock(WebElement.class);
+        when(driver.findElement(any())).thenReturn(mockElement1);
+        WebElement component = testSubject.findElement(By.id("some"));
+        assertEquals(mockElement1, component);
+    }
+
+    @Test
+    void mapElement() {
+        WebElement element = mock(WebElement.class);
+        WebComponent component = testSubject.mapElement(element);
+        assertEquals(element, component.getElement());
+    }
+
+    @Test
+    void getPageSource() {
+        testSubject.getPageSource();
+        verify(driver, only()).getPageSource();
+    }
+
+    @Test
+    void close() {
+        testSubject.close();
+        verify(driver, only()).close();
+    }
+
+    @Test
+    void quit() {
+        testSubject.quit();
+        verify(driver, only()).quit();
+    }
+
+    @Test
+    void getWindowHandles() {
+        testSubject.getWindowHandles();
+        verify(driver, only()).getWindowHandles();
+    }
+
+    @Test
+    void getWindowHandle() {
+        testSubject.getWindowHandle();
+        verify(driver, only()).getWindowHandle();
+    }
+
+    @Test
+    void switchTo() {
+        testSubject.switchTo();
+        verify(driver, only()).switchTo();
+    }
+
+    @Test
+    void navigate() {
+        testSubject.navigate();
+        verify(driver, only()).navigate();
+    }
+
+    @Test
+    void manage() {
+        testSubject.manage();
+        verify(driver, only()).manage();
+    }
+}
