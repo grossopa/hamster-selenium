ba19c6fcf505e73e657def724dd8420d6f277495
Jack Yin <grossopaforever@gmail.com>
2021-08-01 22:57:57 +0800

    WIP fix some code smells of MuiGrid

--- hamster-selenium-component-materialui/src/main/java/com/github/grossopa/selenium/component/mui/core/MuiGrid.java
+++ hamster-selenium-component-materialui/src/main/java/com/github/grossopa/selenium/component/mui/core/MuiGrid.java
@@ -1,20 +1,44 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the The MIT License (MIT) (the "License");
+ *  You may obtain a copy of the License at
+ *
+ *         https://mit-license.org/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.github.grossopa.selenium.component.mui.core;
 
 import com.github.grossopa.selenium.component.mui.AbstractMuiComponent;
 import com.github.grossopa.selenium.component.mui.config.MuiConfig;
 import com.github.grossopa.selenium.core.ComponentWebDriver;
-import com.github.grossopa.selenium.core.component.WebComponent;
 import org.openqa.selenium.WebElement;
+
+import static com.github.grossopa.selenium.component.mui.config.MuiConfig.ATTR_CLASS;
 
 /**
  * The grid creates visual consistency between layouts while allowing flexibility across a wide variety of designs.
  * Material Design’s responsive UI is based on a 12-column grid layout.
  *
- * @author: Elena Wang
+ * @author Elena Wang
+ * @since 1.6
  * @see <a href="https://material-ui.com/components/grid/">
  * https://material-ui.com/components/grid/</a>
- * @date: 7/22/2021 10:02 PM
- * @since: 1.6
  */
 public class MuiGrid extends AbstractMuiComponent {
 
@@ -27,8 +51,8 @@ public class MuiGrid extends AbstractMuiComponent {
      * Constructs an instance with the delegated element and root driver
      *
      * @param element the delegated element
-     * @param driver  the root driver
-     * @param config  the Material UI configuration
+     * @param driver the root driver
+     * @param config the Material UI configuration
      */
     protected MuiGrid(WebElement element, ComponentWebDriver driver, MuiConfig config) {
         super(element, driver, config);
@@ -39,25 +63,33 @@ public class MuiGrid extends AbstractMuiComponent {
         return COMPONENT_NAME;
     }
 
+    /**
+     * TODO add comments for public method please
+     *
+     * @return
+     */
     public boolean isContainer() {
-        String className = this.getAttribute("class");
-        return className.contains("MuiGrid-container");
+        return this.attributeContains(ATTR_CLASS, config.getCssPrefix() + "Grid-container");
     }
 
+    /**
+     * TODO add comments for public method please
+     *
+     * @return
+     */
     public boolean isItem() {
-        String className = this.getAttribute("class");
-        return className.contains("MuiGrid-item");
+        return this.attributeContains(ATTR_CLASS, config.getCssPrefix() + "Grid-item");
     }
 
     // Temp comment as building error
-//    /**
-//     * @param num the number defined in front
-//     * @return true if it is a Grid component
-//     */
-//    public Boolean checkGridItemSpacingValue(int num) {
-//        int spaceNumber = num * 4;
-//
-//    }
+    //    /**
+    //     * @param num the number defined in front
+    //     * @return true if it is a Grid component
+    //     */
+    //    public Boolean checkGridItemSpacingValue(int num) {
+    //        int spaceNumber = num * 4;
+    //
+    //    }
 
     @Override
     public String toString() {
@@ -65,6 +97,7 @@ public class MuiGrid extends AbstractMuiComponent {
     }
 
     /**
+     * Returns true if it is a Grid component
      *
      * @return true if it is a Grid component
      */
